d9584df75edb6f27fe1cfec82dac31733ec49eb1
Abdallah Abdillah <waploaj@icloud.com>
2022-01-11 04:22:26 +0300

    kuhusu functionTypes

--- src/main/kotlin/higherOrderFunction/functionType.kt
+++ src/main/kotlin/higherOrderFunction/functionType.kt
@@ -69,6 +69,73 @@ fun <T>Collection<T>.joinToStrings(
     return result.toString()
 }
 
+//Define a function that return another function
+enum class Delivery{STANDARD, EXPEDITED}
+class Order(val itemcount:Int)
+
+fun getShippingCostCalculator(delivery: Delivery):(Order) ->Double{
+    if (delivery == Delivery.EXPEDITED){
+        return {order -> 6 +2.1 * order.itemcount  }
+    }
+    return {order -> 1.2 * order.itemcount  }
+}
+
+//Using a function that return function in UI code
+data class Person (
+    val firstname:String,
+    val lastname:String,
+    val phoneNumber:String?
+    )
+class ContactListFilter(
+    var prefix: String = " ",
+    var onlyWithPhoneNumber:Boolean = false
+){
+    fun predict():(Person)->Boolean{
+        val startwithprefix = {p:Person ->
+            p.firstname.startsWith(prefix) || p.lastname.startsWith(prefix)
+        }
+        if (!onlyWithPhoneNumber){
+            return startwithprefix
+        }
+        return {startwithprefix(it) && it.phoneNumber !=null}
+    }
+}
+
+//Removing duplication through lambdas
+
+//Define the site visit data
+data class SiteVisit(
+    val path:String,
+    val duration:Double,
+    val os:OS
+)
+enum class OS{WINDOWS,MAC, IOS, ANDROID}
+val log = listOf<SiteVisit>(
+    SiteVisit("/", 34.0, OS.WINDOWS),
+    SiteVisit("/", 22.0, OS.MAC),
+    SiteVisit("/login", 12.0, OS.WINDOWS),
+    SiteVisit("/signup", 8.0, OS.IOS),
+    SiteVisit("/", 16.3, OS.ANDROID)
+)
+//Analyse site visit data with hard-coded filter
+val avergeWindowDuration = log
+    .filter { it.os == OS.WINDOWS }
+    .map (SiteVisit::duration)
+    .average()
+//Now suppose you need statistics for mac user, To remove duplication we extract platform as parameter
+//Removing duplication with a regular function
+fun List<SiteVisit>.averageDuration(os: OS)=
+    filter { it.os == os }.map(SiteVisit::duration).average()
+//This is not powerful imagine we wants statistics for the mobile platform and we have ios and Android
+//Analyze site visit data with complex hard-coded filter
+val averageMobileDuration= log
+    .filter { it.os in setOf(OS.ANDROID,OS.IOS) }
+    .map(SiteVisit::duration)
+    .average()
+//Removing duplication with high-order function
+fun List<SiteVisit>.averageDurationFor(predicate:(SiteVisit) -> Boolean) =
+    filter(predicate).map(SiteVisit::duration).average()
+
 fun main(){
     twoAndThree(sums)
     sum(2,4)
@@ -76,6 +143,18 @@ fun main(){
     println("aba7bc".filter { it in 'a'..'z' })
     val letter = listOf("alpha", "omega")
     println(letter.joinToString(){it.lowercase()})
+
+    val contact = listOf<Person>(Person("Naima","Mohammedi", "0712000000"),
+        Person("Aisha", "Daudi", null)
+    )
+    val contactListFilter = ContactListFilter()
+    with(contactListFilter){
+        prefix = "Naim"
+        onlyWithPhoneNumber = true
+    }
+    println(contact.filter(contactListFilter.predict()))
+    println(log.averageDurationFor { it.os in setOf<OS>(OS.IOS, OS.ANDROID) })
+    println(log.averageDurationFor { it.os == OS.IOS && it.path == "/signup" })
 
 
 }
